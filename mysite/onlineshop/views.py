0ffd437f0bc1dcc375ca7f6cc3201a7872b37a5a
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-16 18:19:55 +0300

    + puslapiavima idejau i products.html ir orders.html

--- mysite/onlineshop/views.py
+++ mysite/onlineshop/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from .models import Product, Order
 from django .shortcuts import render, get_object_or_404
 from django.views import generic
+from django.core.paginator import Paginator
 
 # Create your views here.
 def products(request):
+    paginator = Paginator(Product.objects.all(), 6)
+    page_number = request.GET.get('page')
+    paged_products = paginator.get_page(page_number)
     context = {
-        "products": Product.objects.all()
+        "products": paged_products,
     }
     return render(request, "products.html", context=context)
 
@@ -20,6 +24,7 @@ class OrderListView(generic.ListView):
     model = Order
     template_name = "orders.html"
     context_object_name = 'orders'
+    paginate_by = 4
 
 class OrderDetailView(generic.DetailView):
     model = Order
