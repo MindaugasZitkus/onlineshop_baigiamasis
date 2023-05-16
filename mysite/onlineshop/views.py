b7f1633758e328a1a220a231cd07f6d11ed20a90
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-16 14:23:57 +0300

    + vieno produkto atvaizdavimas

--- mysite/onlineshop/views.py
+++ mysite/onlineshop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django .shortcuts import render, get_object_or_404
 
 # Create your views here.
 def products(request):
@@ -7,3 +8,9 @@ def products(request):
         "products": Product.objects.all()
     }
     return render(request, "products.html", context=context)
+
+def product(request, product_id):
+    context = {
+        "product": get_object_or_404(Product, pk=product_id)
+    }
+    return render(request, 'product.html', context=context)
