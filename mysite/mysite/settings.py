2253fd101cb8df4d15983ac479eace854521deb3
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-05-17 14:33:43 +0300

    + pridejau prisijungti atsijungti mygtukus kurie redirectina i pagrindini ir i login html puslapius

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'onlineshop/media')
 
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
