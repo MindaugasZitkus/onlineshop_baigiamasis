eb350c65e3f1eb36cb3e02d87fda9b30fef33791
Mindaugas Zitkus <zitkusmindaugas@gmail.com>
2023-06-07 09:53:54 +0300

    + sutvarkytas add to cart kad pridetu tik viena preke
     (pries tai pridejo po 3 nes cart.html buvo dar 2 cart.js scriptai ideti testavimui ir automatiskai eme po tris prekes paspaudus viena karta add to cart )
    
     + pridetas add to cart vidiniame produkto puslapyje kur yra aprasymas

--- mysite/onlineshop/static/js/cart.js
+++ mysite/onlineshop/static/js/cart.js
@@ -65,21 +65,20 @@ function addCookieItem(productId, action, csrftoken) {
 }
 
 function getCookie(name) {
-    // Split cookie string and get all individual name=value pairs in an array
+    // Padalina slapuku eilute ir gauna visas atskiras vardas = vertes poras masyve
     var cookieArr = document.cookie.split(";");
 
-    // Loop through the array elements
+    // Pereina per masyvo elementus
     for(var i = 0; i < cookieArr.length; i++) {
         var cookiePair = cookieArr[i].split("=");
 
-        /* Removing whitespace at the beginning of the cookie name
-        and compare it with the given string */
+
         if(name == cookiePair[0].trim()) {
-            // Decode the cookie value and return
+            // Issifruoja slapuko verte ir grazina
             return decodeURIComponent(cookiePair[1]);
         }
     }
 
-    // Return null if not found
+    // grazina 0 jei nieko neranda grazina nuline reiksme
     return null;
 }
